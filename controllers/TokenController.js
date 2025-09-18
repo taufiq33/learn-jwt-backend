742d1a1bc412106b39b36289de1af875067dad8a
Taufiq33 <taufiqhidayat1908@gmail.com>
2025-09-18 07:17:16 +0700

    refactor: using  jwt.validate function with promise based

--- controllers/TokenController.js
+++ controllers/TokenController.js
@@ -3,51 +3,48 @@ import { RefreshTokensModel } from "../models/RefreshTokensModel.js";
 
 import { generateAccessToken, validateJwt } from "../utils/token.js";
 
-export function requestNewAccessToken(req, res) {
+export async function requestNewAccessToken(req, res) {
   const refreshToken = req.cookies?.["refresh-token"];
   if (!refreshToken) {
     return res.status(401).json({ msg: "not authorized" });
   }
 
   try {
-    validateJwt(refreshToken, "refresh", async (error, decoded) => {
-      if (error && error.name === "JsonWebTokenError") {
-        return res.status(401).json({ msg: "invalid token" });
-      }
-
-      if (error && error.name === "TokenExpiredError") {
-        return res.status(401).json({ msg: "expired token" });
-      }
-
-      const sessionExist = await RefreshTokensModel.findOne({
-        where: {
-          userId: decoded.id,
-          token: refreshToken,
-          isRevoked: false,
-          expiredAt: {
-            [Op.gt]: new Date(),
-          },
+    const decoded = await validateJwt(refreshToken, "refresh");
+    const sessionExist = await RefreshTokensModel.findOne({
+      where: {
+        userId: decoded.id,
+        token: refreshToken,
+        isRevoked: false,
+        expiredAt: {
+          [Op.gt]: new Date(),
         },
-      });
+      },
+    });
 
-      if (!sessionExist) {
-        return res.status(401).json({ msg: "invalid/revoked/expired token" });
-      }
+    if (!sessionExist) {
+      return res.status(401).json({ msg: "invalid/revoked/expired token" });
+    }
 
-      const newAccessToken = generateAccessToken({
-        email: decoded.email,
-        username: decoded.username,
-        id: decoded.id,
-      });
+    const newAccessToken = generateAccessToken({
+      email: decoded.email,
+      username: decoded.username,
+      id: decoded.id,
+    });
 
-      return res.json({
-        msg: "acc token regenerated",
-        access_token: newAccessToken,
-      });
+    return res.json({
+      msg: "acc token regenerated",
+      access_token: newAccessToken,
     });
   } catch (error) {
-    console.log(error);
-    console.log(error.name);
+    if (error && error.name === "JsonWebTokenError") {
+      return res.status(401).json({ msg: "invalid token" });
+    }
+
+    if (error && error.name === "TokenExpiredError") {
+      return res.status(401).json({ msg: "expired token" });
+    }
+
     return res.status(error.code || 500).json({
       msg:
         error.errors?.map((item) => item.message) ||
